cd9294d87af085f47a54480de532d39bba3ee2e0
Lily Li <lily.li0597@gmail.com>
2019-01-28 11:38:25 -0500

    commit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,33 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length === 0) {
+		return false;
+	} else if (array.length === 1) {
+		if (array[0] === target) {
+			return true 
+		} else {
+			return false
+		}
+	} else if (array.length === 2) {
+		if (array[0] === target) {
+			return true
+		} else if (array[1] === target) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		const mid = Math.floor(array.length / 2);
+
+		if (array[mid] < target) {
+			return binarySearch(array.slice(0, mid), target)
+		} else if (array[mid] === target) {
+			return true
+		} else {
+			return binarySearch(array.slice(mid + 1, array.length), target)
+		}
+	}
 };
 
 /*
